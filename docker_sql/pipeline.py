a53eda60ed84d9b04cf9799dae7090cd82e9e06b
maning <maning306@hotmail.com>
2023-02-26 14:39:22 -0700

    updated

--- docker_sql/pipeline.py
+++ docker_sql/pipeline.py
@@ -1,5 +1,11 @@
-import pandas as pd 
+import sys
 
-# some fancy stuff with pandas
+import pandas as pd
 
-print('job finished successfully')
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fanc stuff with pandas
+
+print(f'job finished successfully for day = {day}')
